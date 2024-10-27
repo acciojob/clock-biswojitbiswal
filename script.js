50d3c8e50b68febf34ca29c2c578b4733b62b2e3
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-27 10:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let para = document.getElementById("timer");
+
+function setCurrTime() {
+	para.textContent = new Date().toLocaleString();
+};
+
+setCurrTime();
+
+setInterval(setCurrTime, 1000); 
